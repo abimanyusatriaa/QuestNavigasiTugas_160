a6977f60a2d8fe6c67190d108e2a719ba00e42d7
abimanyusatriaa <adji3890@gmail.com>
2025-11-08 22:42:17 +0700

    membuat fun list peserta

---
+++ app/src/main/java/com/example/tugas5/tes/Daftar.kt
@@ -0,0 +1,24 @@
+package com.example.tugas5.tes
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tugas5.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListPeserta(
+    onBerandaClick: () -> Unit = {},
+    onFormulirClick: () -> Unit = {}
+)
+}
