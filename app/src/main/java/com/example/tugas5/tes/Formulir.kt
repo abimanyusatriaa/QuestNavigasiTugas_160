96aeef34d7784c6e33f61272b87c401c10aa6d6b
abimanyusatriaa <adji3890@gmail.com>
2025-11-08 22:47:34 +0700

    membuat val dan var

---
+++ app/src/main/java/com/example/tugas5/tes/Formulir.kt
@@ -0,0 +1,34 @@
+package com.example.tugas5.tes
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsi(
+    modifier: Modifier = Modifier,
+    onBerandaClick: () -> Unit = {}
+) {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
+
+    var showDialog by remember { mutableStateOf(false) }
+
+    val gender = listOf("Laki-laki", "Perempuan")
+    val statusPerkawinan = listOf("Janda", "Lajang", "Duda")
+
+
+}
